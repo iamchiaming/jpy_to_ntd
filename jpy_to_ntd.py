5afe546cd99d42603f82958bc44b2c31a1972c60
Chia-Ming Chang <info@chiamingchang.com>
2021-02-03 15:50:34 +0900

    add more countries

--- jpy_to_ntd.py
+++ jpy_to_ntd.py
@@ -1,4 +1,10 @@
 jpy = input('請輸日圓:')
 jpy = int(jpy)
-ntd = jpy * 0.2681
+ntd = jpy * 0.2785
 print('台幣: ', ntd)
+gbp = jpy * 0.0071
+print('英鎊: ', gbp)
+eur = jpy * 0.0083
+print('歐元: ', eur)
+uad = jpy * 0.0092
+print('美元: ', uad)
